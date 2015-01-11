5fb34909656c823b208c779fbf6feafcbe46f4b7
19RyanA <ryanadolf123@gmail.com>
2015-01-10 18:42:25 -0800

    Added seeding for users and turned off production features
    Added seeding for users table, got rid of the "Application in
    production" messages, and changed to debug mode

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::connection('auth')->table('users')->delete();
+
+		DB::connection('auth')->table('users')->insert(array(
+			array(
+				'username' => '27JohnD',
+				'password' => Hash::make('Password'),
+				'type' => 'student'
+			),
+			array(
+				'username' => 'TeacherX',
+				'password' => Hash::make('teacher!'),
+				'type' => 'teacher'
+			),
+			array(
+				'username' => 'aParent',
+				'password' => Hash::make('pAsWoRd'),
+				'type' => 'parent'
+			),
+			array(
+				'username' => 'admin',
+				'password' => Hash::make('wordPass'),
+				'type' => 'admin'
+			),
+			array(
+				'username' => 'SuperAdmin',
+				'password' => Hash::make('anotherPassword'),
+				'type' => 'superadmin'
+			)
+		));
+
+		$otherTables = DB::connection('auth')->select("select TABLE_NAME from INFORMATION_SCHEMA.TABLES where TABLE_NAME != 'users' and TABLE_NAME != 'migrations' and TABLE_SCHEMA = 'hms'");
+		foreach($otherTables as &$descriptor) {
+			DB::connection('auth')->statement('drop table '.$descriptor->TABLE_NAME);
+		}
+		$users = DB::connection('auth')->select("select id,type from users");
+		$student = 0;
+		foreach($users as &$descriptor) {
+			$tableName = 'user'.$descriptor->id;
+			if($descriptor->type == 'student') $student = $descriptor->id;
+			if($descriptor->type == 'student' || $descriptor->type == 'teacher') {
+				Schema::connection('auth')->create($tableName, function($table) {
+					$table->string('class');
+					$table->tinyInteger('period');
+				});
+				DB::connection('auth')->table($tableName)->insert(array(
+					array(
+						'class' => 'Spanish 2M',
+						'period' => 1
+					)
+				));
+			} else if($descriptor->type == 'parent') {
+				Schema::connection('auth')->create($tableName, function($table) {
+					$table->integer('student');
+				});
+				DB::connection('auth')->table($tableName)->insert(array(
+					array(
+						'student' => $student
+					)
+				));
+			}
+		}
+	}
+
+}
